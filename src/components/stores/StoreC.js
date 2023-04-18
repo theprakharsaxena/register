a9be5c3ccdfca9828695cd73a18b7df649702ca3
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-04-18 13:47:29 +0530

    update

--- src/components/stores/StoreC.js
+++ src/components/stores/StoreC.js
@@ -10,6 +10,7 @@ import { createTheme, styled } from "@mui/material/styles";
 import { ThemeProvider } from "@emotion/react";
 import { Button, Typography } from "@mui/material";
 import Axios from "axios";
+import EditStaffForm from "../forms/EditStaffForm";
 
 const theme = createTheme({
   palette: {
@@ -32,15 +33,36 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 export default function StoreC() {
   const [data, setData] = React.useState([]);
-
-  const getData = async () => {
-    const response = await Axios.get("http://localhost:5000/api/v1/staff");
-    setData(response.data);
-  };
+  // const [response1, setResponse1] = React.useState([]);
+  const [role, setRole] = React.useState("No Role");
+  const businessIds = "kbktbFmdvENXoEriN0UD7VboJET2";
 
   React.useEffect(() => {
     getData();
   }, []);
+
+  const getData = () => {
+    Axios.post(
+      "http://stock.staging.digitalregister.in:8080/api/v1/staff/get",
+      {
+        businessIds: [businessIds],
+      }
+    ).then((res) => setData(res.data.response));
+  };
+
+  function deleteStaff(staffId) {
+    fetch(
+      `http://stock.staging.digitalregister.in:8080/api/v1/staff/delete/${staffId}`,
+      {
+        method: "DELETE",
+      }
+    ).then((result) => {
+      result.json().then((res) => {
+        console.warn(res);
+        getData();
+      });
+    });
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -55,76 +77,69 @@ export default function StoreC() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.filter((data) => data.selectStore === "Store C").map(
-              ({ _id, staffName, mobileNumber, selectStore, selectRole }) => (
-                <TableRow
-                  key={_id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {staffName}
-                  </TableCell>
-                  <TableCell align="right">{mobileNumber}</TableCell>
-                  <TableCell align="left">{selectRole}</TableCell>
-                  <TableCell align="right">
-                    <Typography>
-                      <Button
-                        variant="outlined"
-                        sx={{
-                          textTransform: "none",
-                          borderRadius: " 4px",
-                          color: "#000000",
-                          borderColor: "#000000",
-                          mx: 0.5,
-                        }}
-                        size="small"
-                      >
-                        Change Role
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        sx={{
-                          textTransform: "none",
-                          borderRadius: " 4px",
-                          color: "#000000",
-                          borderColor: "#000000",
-                          mx: 0.5,
-                        }}
-                        size="small"
-                      >
-                        Remove Role
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        sx={{
-                          textTransform: "none",
-                          borderRadius: " 4px",
-                          color: "#000000",
-                          borderColor: "#000000",
-                          mx: 0.5,
-                        }}
-                        size="small"
-                      >
-                        Rename Staff
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        sx={{
-                          textTransform: "none",
-                          borderRadius: " 4px",
-                          color: "#FF0202",
-                          borderColor: "#FF0202",
-                          mx: 0.5,
-                        }}
-                        size="small"
-                      >
-                        Delete Staff
-                      </Button>
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-              )
-            )}
+            {data.map(({ staffId, name, mobile, businessId }) => (
+              <TableRow
+                key={staffId}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {name}
+                </TableCell>
+                <TableCell align="right">{mobile}</TableCell>
+                <TableCell align="left">{role}</TableCell>
+
+                <TableCell align="right">
+                  <Typography>
+                    <Button>
+                      <EditStaffForm
+                        currentRole="Change Role"
+                        currentStaffId={staffId}
+                        currentName={name}
+                        currentCode={mobile.slice(0, 3)}
+                        currentMobile={mobile.slice(3)}
+                      />
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      sx={{
+                        textTransform: "none",
+                        borderRadius: " 4px",
+                        color: "#000000",
+                        borderColor: "#000000",
+                        mx: 0.5,
+                      }}
+                      size="small"
+                    >
+                      Remove Role
+                    </Button>
+                    <Button>
+                      <EditStaffForm
+                        currentRole="Rename Role"
+                        currentStaffId={staffId}
+                        currentName={name}
+                        currentCode={mobile.slice(0, 3)}
+                        currentMobile={mobile.slice(3)}
+                        currentBusinessId={businessId}
+                      />
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      sx={{
+                        textTransform: "none",
+                        borderRadius: " 4px",
+                        color: "#FF0202",
+                        borderColor: "#FF0202",
+                        mx: 0.5,
+                      }}
+                      size="small"
+                      onClick={() => deleteStaff(staffId)}
+                    >
+                      Delete Staff
+                    </Button>
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>

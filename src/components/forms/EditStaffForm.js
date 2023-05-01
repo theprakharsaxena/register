9d4526742d9c98a08534576ded86e778418c474c
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-05-01 12:36:32 +0530

    edit staff reload

--- src/components/forms/EditStaffForm.js
+++ src/components/forms/EditStaffForm.js
@@ -1,7 +1,5 @@
 import * as React from "react";
 import {
-
-  
   Grid,
   Button,
   Box,
@@ -68,11 +66,6 @@ export default function EditStaffForm({
     setOpen(false);
   };
 
-  const handleUpdateandClose = () => {
-    updateStaff();
-    handleClose();
-  };
-
   const updateStaff = () => {
     Axios.post(
       "http://stock.staging.digitalregister.in:8080/api/v1/staff/update",
@@ -86,6 +79,11 @@ export default function EditStaffForm({
       .then((response) => console.log(response.data))
       .catch((err) => console.log(err));
   };
+
+  function handleUpdateandClose(e) {
+    updateStaff();
+    setOpen(!open);
+  }
 
   return (
     <div>
@@ -117,140 +115,142 @@ export default function EditStaffForm({
           </Button>
         </Grid>
         <Box sx={{ bgcolor: "#F5F5F5", p: 2.5 }}>
-          <form method="put"></form>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <Typography sx={{ color: "black" }}>Staff Name*</Typography>
-              <TextField
-                required
-                fullWidth
-                placeholder="Enter here"
-                name="staffName"
-                variant="outlined"
-                value={staffName}
-                onChange={(e) => setStaffName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={6} container spacing={1}>
-              <Grid item xs={3}>
-                <Typography sx={{ color: "black", maxWidth: 10 }}>
-                  Code
-                </Typography>
-                <TextField
-                  required
-                  placeholder="Select"
-                  name="countryCode"
-                  variant="outlined"
-                  defaultValue="+91"
-                  value={countryCode}
-                  onChange={(e) => setCountryCode(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={9}>
-                <Typography sx={{ color: "black" }}>Mobile Number*</Typography>
+          <form onSubmit={(e) => handleUpdateandClose(e)}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Typography sx={{ color: "black" }}>Staff Name*</Typography>
                 <TextField
                   required
                   fullWidth
                   placeholder="Enter here"
-                  name="mobileNumber"
+                  name="staffName"
                   variant="outlined"
-                  value={mobileNumber}
-                  onChange={(e) => setMobileNumber(e.target.value)}
+                  value={staffName}
+                  onChange={(e) => setStaffName(e.target.value)}
                 />
               </Grid>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography sx={{ color: "black" }}>Select Store*</Typography>
-              <TextField
-                select
-                required
-                fullWidth
-                placeholder="Select"
-                name="selectStore"
-                variant="outlined"
-                // value={selectStore}
-                // onChange={(e) => setSelectStore(e.target.value)}
-              >
-                {selectStoreDropdown.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography sx={{ color: "black" }}>Select Role*</Typography>
-              <TextField
-                select
-                required
-                fullWidth
-                placeholder="Select"
-                name="selectRole"
-                variant="outlined"
-                // value={selectRole}
-                // onChange={(e) => setSelectRole(e.target.value)}
-              >
-                {selectRoleDropdown.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-          </Grid>
-          <Paper sx={{ minHeight: 205, my: 3, p: 1.5 }}>
-            <Typography>
-              <Typography sx={{ fontWeight: "bold" }}>
-                Store Admin Permissions -
-              </Typography>
-              <Box
-                sx={{ m: -2, p: -2, fontWeight: "lighter", fontSize: "15px" }}
-              >
-                <ul>
-                  <li>View all entries & download reports</li>
-                  <li>Add, edit, delete any type of entry</li>
-                  <li>View total sale, purchase</li>
-                  <li>
-                    View all added items, add new item, edit item, delete item
-                  </li>
-                  <li>
-                    Add new party, view all added parties and their entries
-                  </li>
-                  <li>Download & share all reports, bills</li>
-                </ul>
-              </Box>
-            </Typography>
-          </Paper>
-          <Grid container spacing={2}>
-            <Grid item xs={6}></Grid>
-            <Grid item xs={6} container spacing={2}>
+              <Grid item xs={6} container spacing={1}>
+                <Grid item xs={3}>
+                  <Typography sx={{ color: "black", maxWidth: 10 }}>
+                    Code
+                  </Typography>
+                  <TextField
+                    required
+                    placeholder="Select"
+                    name="countryCode"
+                    variant="outlined"
+                    defaultValue="+91"
+                    value={countryCode}
+                    onChange={(e) => setCountryCode(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography sx={{ color: "black" }}>
+                    Mobile Number*
+                  </Typography>
+                  <TextField
+                    required
+                    fullWidth
+                    placeholder="Enter here"
+                    name="mobileNumber"
+                    variant="outlined"
+                    value={mobileNumber}
+                    onChange={(e) => setMobileNumber(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
               <Grid item xs={6}>
-                <Button
-                  type="button"
+                <Typography sx={{ color: "black" }}>Select Store*</Typography>
+                <TextField
+                  select
+                  required
                   fullWidth
+                  placeholder="Select"
+                  name="selectStore"
                   variant="outlined"
-                  size="large"
-                  sx={{ color: "#1602FF", borderColor: "#1602FF" }}
-                  onClick={handleClose}
+                  // value={selectStore}
+                  // onChange={(e) => setSelectStore(e.target.value)}
                 >
-                  Cancel
-                </Button>
+                  {selectStoreDropdown.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Grid>
               <Grid item xs={6}>
-                <Button
-                  type="submit"
+                <Typography sx={{ color: "black" }}>Select Role*</Typography>
+                <TextField
+                  select
+                  required
                   fullWidth
-                  text="Submit"
-                  variant="contained"
-                  size="large"
-                  sx={{ color: "#FFFFFF", bgcolor: "#1602FF" }}
-                  onClick={handleUpdateandClose}
+                  placeholder="Select"
+                  name="selectRole"
+                  variant="outlined"
+                  // value={selectRole}
+                  // onChange={(e) => setSelectRole(e.target.value)}
                 >
-                  Save
-                </Button>
+                  {selectRoleDropdown.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Grid>
             </Grid>
-          </Grid>
+            <Paper sx={{ minHeight: 205, my: 3, p: 1.5 }}>
+              <Typography>
+                <Typography sx={{ fontWeight: "bold" }}>
+                  Store Admin Permissions -
+                </Typography>
+                <Box
+                  sx={{ m: -2, p: -2, fontWeight: "lighter", fontSize: "15px" }}
+                >
+                  <ul>
+                    <li>View all entries & download reports</li>
+                    <li>Add, edit, delete any type of entry</li>
+                    <li>View total sale, purchase</li>
+                    <li>
+                      View all added items, add new item, edit item, delete item
+                    </li>
+                    <li>
+                      Add new party, view all added parties and their entries
+                    </li>
+                    <li>Download & share all reports, bills</li>
+                  </ul>
+                </Box>
+              </Typography>
+            </Paper>
+            <Grid container spacing={2}>
+              <Grid item xs={6}></Grid>
+              <Grid item xs={6} container spacing={2}>
+                <Grid item xs={6}>
+                  <Button
+                    type="button"
+                    fullWidth
+                    variant="outlined"
+                    size="large"
+                    sx={{ color: "#1602FF", borderColor: "#1602FF" }}
+                    onClick={handleClose}
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    text="Submit"
+                    variant="contained"
+                    size="large"
+                    sx={{ color: "#FFFFFF", bgcolor: "#1602FF" }}
+                  >
+                    Save
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </form>
         </Box>
       </Dialog>
     </div>
